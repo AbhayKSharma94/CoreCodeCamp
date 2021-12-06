242d7009de43e139fb396cd49a11f465ae57d2e2
Abhay Sharma <abhayksharma1994@gmail.com>
2021-12-06 18:21:00 -0500

    API now returns models instead of entities

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +13,37 @@ namespace CoreCodeCamp.Controllers
     [Route("api/[controller]")]
     public class CampsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCamps()
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
-            return Ok (new{ Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+            _repository = repository;
+            _mapper = mapper;
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+
+                CampModel[] models = _mapper.Map<CampModel[]>(results);
+                return Ok(models);
+            }
+
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+
+
+
+          
+        }
+
     }
 
 }
