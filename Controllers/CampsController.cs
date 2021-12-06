e052378f8d6b57e6e65ec035322d8ae7b41fd513
Abhay Sharma <abhayksharma1994@gmail.com>
2021-12-06 16:34:27 -0500

    added exceptions

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            return Ok (new{ Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+        }
+    }
+
+}
