4a070d9402e99d50d252ae27fce685eab12228d1
Abhay Sharma <abhayksharma1994@gmail.com>
2021-12-06 19:00:36 -0500

    Returning related data

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -12,7 +12,8 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
+            this.CreateMap<Camp, CampModel>()
+            .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
         }
     }
 }
